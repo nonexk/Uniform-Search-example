76edd9c40ba473d1dc6ef9ec1d76845178c40e4a
nonexk <78900124+nonexk@users.noreply.github.com>
2021-02-11 15:21:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
- 
-// Generate image.
-$im = imagecreatetruecolor(255, 255);
- 
-// Create a colour.
-$white = imagecolorallocate($im, 0, 0, 0);
- 
-// Draw a cirlce in the middle of the image.
-imageellipse($im, 128, 128, 200, 200, $white);
- 
-// Save the image to a file.
-imagepng($im, 'circle.png');
- 
-// Destroy the image handler.
-imagedestroy($im);
+$txt = "PHP";
+echo "I love $txt!";
 ?>
-
-</body>
-</html>
